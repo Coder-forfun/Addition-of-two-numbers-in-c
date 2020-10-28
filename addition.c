4bc3c4100610d6cd58259a3000f464c4fdf5b0e2
Dhairya-PU <73570724+Dhairya-PU@users.noreply.github.com>
2020-10-28 10:31:07 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
 
 int main()
 {
-int a,b,c;
-printf("Enter the two numbers\n");
-scanf("%d%d",&a,&b);
-c=a+b;
-printf("Sum of numbers is %d",c);
+int a,b;
+printf("Enter the first numbers\n");
+scanf("%d",&a);
+  
+printf("Enter the second numbers\n");
+scanf("%d",&b);
+  
+printf("Sum of numbers is %d",a+b);
 return 0;
 }
